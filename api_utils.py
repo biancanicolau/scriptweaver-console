3702188805a7a485cde95540d4d732d3655dea28
biancanicolau <biancanicolau661@gmail.com>
2025-07-29 16:56:19 +0300

    Update api_utils.py

--- api_utils.py
+++ api_utils.py
@@ -9,3 +9,18 @@ def get_current_ip():
     except Exception as e:
         return f"Error: {str(e)}"
 
+def get_random_joke():
+    try:
+        url = "https://v2.jokeapi.dev/joke/Any"
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+
+        if data["type"] == "single":
+            return data["joke"]
+        else:
+            return f"{data['setup']}\n{data['delivery']}"
+    except Exception as e:
+        return f"Error fetching joke: {str(e)}"
+
+
